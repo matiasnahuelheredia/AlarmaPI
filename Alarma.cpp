4886f4524ba2239bf909ad886d4cae6a8e9ae8ee
matiasnahuelheredia <matias@informaticos.com>
2018-06-04 18:36:22 -0300

    add mac detection

--- Alarma.cpp
+++ Alarma.cpp
@@ -5,19 +5,13 @@
 // Copyright   : GPL V2
 // Description : Hello World in C, Ansi-style
 //============================================================================
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <tins/tins.h>
 #include <iostream>
-#include <tins/tins.h>
 #include <string>
-
 #include <tins/tins.h>
-#include <iostream>
 #include <set>
-#include <string>
-
 
 using namespace Tins;
 
@@ -28,27 +22,15 @@ std::set<HWAddress<6>> addrs;
 const HWAddress<3> expected_oui("00:50:F2");
 
 bool handler(const PDU& pdu) {
-    const Dot11Beacon& beacon = pdu.rfind_pdu<Dot11Beacon>();
     // Only process it once
-    if(addrs.insert(beacon.addr3()).second) {
-        // Iterate the tagged options
-        for(const auto& opt : beacon.options()) {
-            // Is this a vendor-specific tag?
-            if(opt.option() == Dot11::VENDOR_SPECIFIC) {
-                // Make sure there's enough size for the OUI + identifier
-                if(opt.data_size() >= 4) {
-                    // Retrieve the OUI field
-                    HWAddress<3> addr = opt.data_ptr();
-                    // Are we interested in this OUI and is it a WPS tag?
-                    if(addr == expected_oui && opt.data_ptr()[3] == 0x04) {
-                        std::cout << "[+] Access point: " << beacon.ssid() << " uses WPS\n";
-                    }
-                }
-            }
-        }
-    }
+    const Dot11Data &ip = pdu.rfind_pdu<Dot11Data>(); // Find the IP layer
+    std::cout << ip.src_addr() << " -> "
+              << ip.dst_addr() << ':' << std::endl;
+
     return true;
 }
+
+
 
 int main(int argc, char* argv[]) {
     if(argc != 2) {
@@ -58,8 +40,9 @@ int main(int argc, char* argv[]) {
     // Only sniff beacons
     SnifferConfiguration config;
     config.set_snap_len(2000);
+    config.set_rfmon(true);
     config.set_promisc_mode(true);
-    config.set_filter("wlan type mgt subtype beacon");
+    //config.set_filter("wlan type mgt subtype beacon");
     Sniffer sniffer(argv[1], config);
     sniffer.sniff_loop(handler);
 }
